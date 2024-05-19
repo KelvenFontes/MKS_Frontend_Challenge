7ab7c5a21799098552049aa222eb03dafe715dbe
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-05-19 20:04:24 -0300

    feat: add react-query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,29 @@
 
 import Footer from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { useQuery } from "react-query";
 
 export default function Home() {
+
+  const { data, isLoading, isError } = useQuery(['products'], () =>
+    fetch('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC')
+      .then(response => response.json())
+  );
+
   return (
     <>
       <Header />
+      <main>
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error loading products</p>}
+        {data && (
+          <ul>
+            {data?.products.map(product => (
+              <li key={product.id}>{product.name}</li>
+            ))}
+          </ul>
+        )}
+      </main>
       <Footer />
     </>
   );
---
+++ src/util/Providers.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ReactNode } from "react"
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { useState } from "react";
+import { GlobalStyles } from "@/app/GlobalStyles";
+
+export default function Provider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      {children}
+    </QueryClientProvider>
+  );
+}
