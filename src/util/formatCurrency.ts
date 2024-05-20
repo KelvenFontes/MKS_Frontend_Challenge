4cdab137fdd79a94f712dcb7f7ee1cc3d1cd911e
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-05-19 21:58:05 -0300

    feat: create function format value

---
+++ src/util/formatCurrency.ts
@@ -0,0 +1,21 @@
+const formatarValor = (valor: string) => {
+
+  if (typeof valor !== 'string') {
+    return 'Valor inválido';
+  }
+
+  const valorNumerico = parseFloat(valor);
+
+  const valorFormatado = valorNumerico.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  });
+
+  const valorSemCentavos = valorFormatado.replace(',00', '').replace(' ', '');
+
+  console.log(valorSemCentavos);
+  return valorSemCentavos;
+}
+
+export default formatarValor;
